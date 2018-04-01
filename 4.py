8f850b4ee7d3098093e515cbb32e640e6751d0c6
Lxy <18340899076@163.com>
2018-04-01 20:13:24 +0800

    Desktop file

---
+++ 4.py
@@ -0,0 +1,19 @@
+                if age>100:
+                    print("百岁老人")
+                else:
+                    print("健康")
+                if age>100:
+                if age>100:
+                if age>100:
+                if age>100:
+                    print("百岁老人")
+                else:
+                    print("健康")
+                if age>100:
+                    print("百岁老人")
+                else:
+                    print("健康")
+                if age>100:
+                    print("百岁老人")
+                else:
+                    print("健康")

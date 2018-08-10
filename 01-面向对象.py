8aca3db7b362f090418049fca4b58709c5ba3090
Lxy <18340899076@163.com>
2018-08-10 17:00:35 +0800

    面向对象

---
+++ 01-面向对象.py
@@ -0,0 +1,22 @@
+class cat:
+    def __init__(self,new_name,new_age):
+        self.name=new_name
+        self.age=new_age
+    def __str__(self):
+        return"%s的年龄是%d"%(self.name,self.age)
+    def eat(face):
+       #print(newface= (face+=1))
+       #print(face)
+        print("吃鱼。。。")
+    def drink(face):
+        print("喝牛奶。。")
+    def word(face):
+        print("%s的年龄是:%d"%(tom.name, tom.age))
+    
+
+tom = cat("汤姆",11)
+lanmao = cat("蓝猫",33)
+#调用方法
+#添加属性
+print(tom)
+print(lanmao)
